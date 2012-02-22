f245bac1da58e6d7f7fb968c9fb654aed93dd915
Jan Dudek <jd@jandudek.com>
2012-02-23 00:01:45 +0100

    Lots of additions to the parser

--- src/parser.js
+++ src/parser.js
@@ -1,34 +1,15 @@
-// A parser is a function that takes input (as Stream instance, defined below)
-// and returns a list of pairs: (parsing result, rest of input).
+// A parser is a function that takes input (a string) and returns a list of
+// pairs: (parsing result, rest of input). Pairs are represented as two-element
+// arrays.
 
-// A successful parse is non-empty list.
+// A successful parse is non-empty list. More than one result means more than
+// one possible parse result.
 
 // The simplest parsers accept single characters, but we'll combine them using
 // combinators like sequence() or choice(), finally leading to program() parser
-// which returns abstract syntax tree (AST) for given program.
+// which returns abstract syntax tree (AST) for whole program.
 
-
-// Stream is a wrapper for String objects, used as input to parsers.
-var Stream = function (string, pos) {
-  var pos = pos || 0;
-
-  var peek = function () {
-    return string.charAt(pos);
-  };
-
-  var consume = function () {
-    return new Stream(string, pos + 1);
-  };
-
-  var toString = function () {
-    return string.slice(pos);
-  };
-
-  return {
-    peek: peek, consume: consume, toString: toString
-  }
-};
-
+// TODO describe or remove
 var ret = function (v) {
   return function (input) {
     return [[v, input]];
@@ -46,21 +27,16 @@ var bind = function (p, f) {
   };
 };
 
-var compose = function (f, g) {
-  return function (x) {
-    return f(g(x));
-  };
-};
-
 // Two basic building blocks for parsers are sequence() and choice().
 
 // sequence() accepts a list of parsers and creates new parser which will run
 // them consecutively. The results from each parser are passed
 // to "decorator" which builds a node of AST.
-var sequence = function () {
-  var parsers = Array.prototype.slice.call(arguments, 0);
-  var decorator = parsers.pop();
 
+// decorator functions will not necesseraly use results from all parsers
+// in the sequence. We will add suffix _ to names of such parameters.
+// Later we'll see many examples of this convention.
+var sequence = function (parsers, decorator) {
   var step = function (parsers, results) {
     if (parsers.length > 0) {
       return bind(parsers[0], function (r) {
@@ -75,6 +51,12 @@ var sequence = function () {
   }
 };
 
+// decorate() is a special case of sequence(), but with only one parser given.
+// This saves us from typing [] in such cases.
+var decorate = function (parser, decorator) {
+  return sequence([parser], decorator);
+}
+
 // choice() will try running each of given parsers. It'll return combined
 // results from all of them (as list of alternatives).
 var choice = function (parsers) {
@@ -85,7 +67,8 @@ var choice = function (parsers) {
   };
 };
 
-// many1() accepts many occurences of parsed input, but at least one.
+// many1() accepts many (at least one) occurences of input acceptable by given
+// parser.
 var many1 = function (parser) {
   return bind(parser, function (r) {
     return bind(many(parser), function (rs) {
@@ -94,15 +77,27 @@ var many1 = function (parser) {
   });
 };
 
-// many() accepts just many occurances of parsed input
+// Same as many1(), but allows no occurences.
 var many = function (parser) {
   return choice([ many1(parser), ret([]) ]);
 };
 
-// A parser which always fails.
-var zero = function (input) {
-  return [];
+// sepBy1 accepts many (at least one) occurences of input acceptable by given
+// parser, separated by input parsed by separator.
+// For example: sepBy1(symbol(","), number) will accept "12,34,5".
+var sepBy1 = function (separator, parser) {
+  return sequence([
+    parser, many(
+      sequence([separator, parser], function (s_, x) { return x; })
+    )],
+    function (x, xs) { return [x].concat(xs); }
+  );
 }
+
+// Same as sepBy1, but allows no occurences.
+var sepBy = function (separator, parser) {
+  return choice([ sepBy1(separator, parser), ret([]) ]);
+};
 
 // debug() is a combinator that wraps a parser and prints given input every
 // time wrapped parser is called.
@@ -113,15 +108,17 @@ var debug = function(parser) {
   }
 }
 
+// Now we're finished with combinators. We'll define real parsers.
+
 // The simplest useful parser is character(c). It accepts only if first input
 // character equals c. As all parsers, it returns a list of pairs:
 // (result, remaining input).
 // The result is just the parsed character.
 var character = function (expected) {
   return function (input) {
-    var actual = input.peek();
+    var actual = input.charAt(0);
     if (actual === expected) {
-      input = input.consume();
+      input = input.slice(1);
       return [[actual, input]];
     } else {
       return [];
@@ -129,11 +126,14 @@ var character = function (expected) {
   };
 };
 
+// For given string, returns a parser that will accept and return that string.
 var string = function (str) {
   var parsers = str.split("").map(character);
   var join = function () { return Array.prototype.join.call(arguments, ""); };
-  return sequence.apply(this, parsers.concat([join]));
+  return sequence(parsers, join);
 };
+
+// Below are some simple parsers for single characters.
 
 var letter = choice(
   "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").
@@ -156,19 +156,22 @@ var returnFirst = function (x) { return x; };
 // but will also skip any trailing whitespace and comments.
 var lexeme = function (parser) {
   // TODO skip comments
-  return sequence(parser, whiteSpace, function (r) { return r; });
+  return sequence([parser, whiteSpace], returnFirst);
 };
 
-// And identifier starts with a letter, followed by letters, digits
+// From now on, every parser will a be lexeme parser or use lexeme parser at
+// its end. In other words, every parser defined below will accept and skip
+// trailing whitespace and comments.
+
+// An identifier starts with a letter, followed by letters, digits
 // or underscores (_).
 var identifier = sequence(
-  letter, many(choice([letter, digit, character("_")])),
+  [letter, many(choice([letter, digit, character("_")]))],
   function (x, xs) { return x + xs.join(""); }
 );
 identifier = lexeme(identifier);
 
-// TODO support float numbers
-var integer = sequence(
+var integer = decorate(
   many1(digit),
   function (ds) { return parseInt(ds.join("")); }
 );
@@ -184,35 +187,219 @@ var operator = function (s) {
   return lexeme(string(s));
 };
 
-var numberLiteral = sequence(integer, function (i) {
-  return { number: i };
+var symbol = function (s) {
+  return lexeme(string(s));
+};
+
+// between() combines three parsers, but returns only results from "inside"
+// parser.
+var between = function (before, after, inside) {
+  return sequence(
+    [before, inside, after],
+    function (b_, i, a_) { return i; }
+  );
+};
+
+// Given existing parser, create a new one for input wrapped in parentheses.
+var parens = function (parser) {
+  return between(symbol("("), symbol(")"), parser);
+};
+
+// Same as parens, but with curly braces: { and }.
+var braces = function (parser) {
+  return between(symbol("{"), symbol("}"), parser);
+};
+
+// Same as parens, but with square brackets: [ and ].
+var squares = function (parser) {
+  return between(symbol("["), symbol("]"), parser);
+};
+
+// Now we start defining parsers that build AST.
+
+// TODO support float numbers
+var numberLiteral = decorate(integer, function (i) {
+  return { numberLiteral: i };
 });
 
-var variable = sequence(identifier, function (i) {
+// This parser is wrapped in a function, executed immediately, because we want
+// to define locally some simpler parsers, not visible outside.
+
+var stringLiteral = function () {
+  // TODO string literals should allow not only letters!
+  // TODO escaping
+  var contents = decorate(many(letter), function (xs) { return xs.join(""); })
+
+  var inSingleQuotes = between(character("'"), character("'"), contents);
+  var inDoubleQuotes = between(character('"'), character('"'), contents);
+
+  return decorate(
+    choice([inSingleQuotes, inDoubleQuotes]),
+    function (string) { return { stringLiteral: string }; }
+  );
+}();
+
+// Many parsers need to be defined recursively. For example, an object literal
+// will contain expression, so we need to use expr parser. However, expr cannot
+// be defined yet, as it will need objectLiteral parser.
+
+// That's why we wrap such recursive parsers in functions.
+
+var objectLiteral = function (input) {
+  var pair = sequence(
+    [identifier, symbol(":"), expr],
+    function (id, s_, expr) { return [id, expr]; }
+  );
+
+  return decorate(
+    braces(sepBy(symbol(","), pair)),
+    function (pairs) { return { objectLiteral: pairs }; }
+  )(input);
+};
+
+var arrayLiteral = function (input) {
+  return decorate(
+    squares(sepBy(symbol(","), expr)),
+    function (exprs) { return { arrayLiteral: exprs }; }
+  )(input);
+};
+
+var variable = decorate(identifier, function (i) {
   return { variable: i };
 });
 
-var expr = sequence(choice([numberLiteral, variable]), function (simple) {
-  return { expression: simple };
-});
+var func = function (input) {
+  var args = parens(sepBy(symbol(","), identifier));
+  var body = braces(many(statement));
 
-// As a convention, when using sequence() we'll add suffix _ to parameters
-// which are not used to build AST node.
-var varStmt = sequence(
-  keyword("var"), expr, operator("="), expr,
-  function (k_, lexpr, o_, rexpr) {
-    return { varStatement: [lexpr, rexpr] };
+  return sequence(
+    [keyword("function"), args, body],
+    function (k_, args, statements) { return { func: [args, statements] }; }
+  )(input);
+};
+
+var expr = function (input) {
+  return choice([
+    numberLiteral,
+    stringLiteral,
+    objectLiteral,
+    arrayLiteral,
+    variable,
+    func,
+    parens(expr)
+  ])(input);
+};
+
+var varStatementWithoutAssignment = sequence(
+  [keyword("var"), identifier],
+  function (k_, id) {
+    return { varStatement: [id] };
   }
 );
 
-// returnFirst means that whole parser will return what was returned
-// from varStmt.
-var statement = sequence(varStmt, lexeme(semicolon), returnFirst);
+var varStatementWithAssignment = sequence(
+  [keyword("var"), identifier, operator("="), expr],
+  function (k_, id, op_, expr) {
+    return { varStatement: [id, expr] };
+  }
+);
 
-// A program is a sequence of statements.
-var program = sequence(many(statement), returnFirst);
+var varStatement = choice([varStatementWithAssignment, varStatementWithoutAssignment]);
 
-// The only function exported from this module.
-exports.parse = function (input) {
-  return program(new Stream(input));
-}
+var assignStatement = sequence(
+  [expr, operator("="), expr],
+  function (lexpr, op_, rexpr) {
+    return { assignStatement: [lexpr, rexpr] };
+  }
+);
+
+var returnStatement = sequence(
+  [keyword("return"), expr],
+  function (k_, expr) {
+    return { returnStatement: expr };
+  }
+);
+
+var ifStatementWithoutElse = function (input) {
+  return sequence(
+    [keyword("if"), parens(expr), braces(many(statement))],
+    function (k_, expr, statements) {
+      return { ifStatement: [expr, statements, []] };
+    }
+  )(input);
+};
+
+var ifStatementWithElse = function (input) {
+  return sequence(
+    [keyword("if"), parens(expr), braces(many(statement)),
+      keyword("else"), braces(many(statement))],
+    function (k_, expr, statements1, k2_, statements2) {
+      return { ifStatement: [expr, statements1, statements2] };
+    }
+  )(input);
+};
+
+var ifStatement = choice([ifStatementWithElse, ifStatementWithoutElse]);
+
+var tryStatement = function (input) {
+  return sequence(
+    [keyword("try"), braces(many(statement)),
+      keyword("catch"), parens(identifier), braces(many(statement))],
+    function (try_, tryStatements, catch_, id, catchStatements) {
+      return { tryStatement: [tryStatements, id, catchStatements] };
+    }
+  )(input);
+};
+
+throwStatement = sequence(
+  [keyword("throw"), expr],
+  function (t_, expr) {
+    return { throwStatement: expr };
+  }
+);
+
+exprStatement = decorate(expr, function (e) {
+  return { exprStatement: e };
+});
+
+var statement = choice([
+    sequence([varStatement,    lexeme(semicolon)], returnFirst),
+    sequence([assignStatement, lexeme(semicolon)], returnFirst),
+    sequence([returnStatement, lexeme(semicolon)], returnFirst),
+    sequence([throwStatement,  lexeme(semicolon)], returnFirst),
+    sequence([exprStatement,   lexeme(semicolon)], returnFirst),
+
+    // if and try statements, unlike others, are not followed by a semicolon.
+    ifStatement,
+    tryStatement,
+
+    // But we want to allow programs with unnecessary semicolons, so we add
+    // "empty" statement that parses to null.
+    decorate(lexeme(semicolon), function () { return null; })
+]);
+
+// A program consists of many statements.
+var program = many1(statement);
+
+// The runner for parsers. By default uses "program" parser.
+// It will apply parser to the input, reject any incomplete parses (i.e. those
+// with any remaining input) and return first AST from the list.
+// This is means that if there is more than one successful parse, all but first
+// one are discarded.
+exports.parse = function (input, parser) {
+  parser = parser || program;
+  var results = parser(input);
+  var completeResults = results.filter(function (result) {
+    var ast = result[0];
+    var rest = result[1];
+    return rest.toString().length == 0;
+  });
+  if (completeResults.length > 0) {
+    return { success: completeResults[0][0] };
+  } else {
+    return { failure: results };
+  }
+};
+
+// Here we export any parsers that we want to test individually.
+exports.expr = expr;
--- test/parser.js
+++ test/parser.js
@@ -1,25 +1,55 @@
 var assert = require("assert");
 var parser = require("parser");
 
-var parse = function (input) {
-  var result = new parser.parse(input);
-  if (result.length > 0) {
-    return { success: result[0][0] };
-  } else {
-    return { failure: [] };
-  }
+var testParser = function (input, expectedAst, specificParser) {
+  assert.deepEqual(parser.parse(input, specificParser), { success: expectedAst });
 };
 
-assert.deepEqual(parse("var x = 5;"), { success: [{
-  varStatement: [
-    { expression: { variable: "x" } },
-    { expression: { number: 5 } },
-  ]
-}] });
+testParser("var x;", [{ varStatement: [ "x" ] }]);
+testParser("var x = 5;", [{ varStatement: [ "x", { numberLiteral: 5 } ] }]);
+testParser("var x_12 = 5;", [{ varStatement: [ "x_12", { numberLiteral: 5 } ] }]);
+testParser("y = 5;", [{ assignStatement: [ { variable: "y" }, { numberLiteral: 5 } ] }]);
+testParser("return 5;", [{ returnStatement: { numberLiteral: 5 } }]);
+testParser("if (x) { return 5; }", [
+  { ifStatement: [
+    { variable: "x"},
+    [{ returnStatement: { numberLiteral: 5 } }],
+    []
+  ]}
+]);
+testParser("if (x) { return 5; } else { return 10; }", [
+  { ifStatement: [
+    { variable: "x"},
+    [{ returnStatement: { numberLiteral: 5 } }],
+    [{ returnStatement: { numberLiteral: 10 } }]
+  ]}
+]);
+testParser("try { return 5; } catch (e) { return e; }", [
+  { tryStatement: [
+    [{ returnStatement: { numberLiteral: 5 } }],
+    "e",
+    [{ returnStatement: { variable: "e" } }]
+  ]}
+]);
+testParser("throw x;", [{ throwStatement: { variable: "x" } }]);
+testParser("var x;;", [{ varStatement: [ "x" ] }, null]);
+testParser("x;", [{ exprStatement: { variable: "x" } }]);
 
-assert.deepEqual(parse("var x_12 = 5;"), { success: [{
-  varStatement: [
-    { expression: { variable: "x_12" } },
-    { expression: { number: 5 } },
-  ]
-}] });
+// tests for expression parser
+testParser("5", { numberLiteral: 5 }, parser.expr);
+testParser("(5)", { numberLiteral: 5 }, parser.expr);
+testParser("x", { variable: "x" }, parser.expr);
+testParser("'abc'", { stringLiteral: "abc" }, parser.expr);
+testParser('"abc"', { stringLiteral: "abc" }, parser.expr);
+testParser("{}", { objectLiteral: [] }, parser.expr);
+testParser("{x: 2}", { objectLiteral: [["x", { numberLiteral: 2 }]] }, parser.expr);
+testParser("{x: 2, y: 3}", { objectLiteral: [
+    ["x", { numberLiteral: 2 }],
+    ["y", { numberLiteral: 3 }]
+  ] }, parser.expr);
+testParser("[]", { arrayLiteral: [] }, parser.expr);
+testParser("[12, x]", { arrayLiteral: [{ numberLiteral: 12 }, { variable: "x" }] }, parser.expr);
+testParser("function (x, y) { return x; }", { func: [
+    ["x", "y"],
+    [{ returnStatement: { variable: "x" } }]
+  ] }, parser.expr);
