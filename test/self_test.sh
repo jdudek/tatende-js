9efbe946e3919cbc446f78021bc850fc341edcef
Jan Dudek <jd@jandudek.com>
2012-05-15 01:20:43 +0200

    Added the ultimate self-test

--- test/self_test.sh
+++ test/self_test.sh
@@ -6,3 +6,5 @@ time ./a.out
 
 ./bin/compile test/parser_test.js "ast=src/ast.js,parser=src/parser.js,assert=src/assert.js" | gcc -xc -
 time ./a.out
+
+./bin/compile src/run.js "ast=src/ast.js,parser=src/parser.js,c_backend=src/c_backend.js,compiler=src/compiler.js" | gcc -m32 -O2 -xc -
